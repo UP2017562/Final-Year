96b71dde5877520ce26b411e0855bf9fd4fd7f8a
Cameron Alexander <91853736+UP2017562@users.noreply.github.com>
2025-04-07 16:35:48 +0100

    MenuBar
    Successfully completed the navbar, will implement movements to different pages soon

--- website/mywebsiteapp.js
+++ website/mywebsiteapp.js
@@ -3,7 +3,7 @@ const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -12,9 +12,12 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home', {title: 'welcome to handlebars'});
+    const model={
+        style: "mystyle.css"
+    }
+    res.render('home.handlebars', model)
 });
 
-app.listen(port, () => {
-    console.log('Server running at http://localhost:${PORT}');
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
